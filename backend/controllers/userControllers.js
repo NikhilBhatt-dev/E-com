77c52f243990696df946b23d1b197c28ed517987
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-03-21 10:56:42 +0530

    login credit

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,31 +1,91 @@
+import validator from "validator";
+import userModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { compare } from "bcrypt";
 
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {});
+};
 
-//Routes for user login
+//routes for user login
 const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    const user = await userModel.findOne({ email });
 
-}    
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Incorrect password" });
+    }
+
+    const token = createToken(user._id);
+
+    res.json({
+      success: true,
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //routes for user register
 const registerUser = async (req, res) => {
-   try{
-    const {name,email,password} = req.body;
+  try {
+    const { name, email, password } = req.body;
 
     //checking user alerdy exits or not
-    const exits = await userModel.findOne({email})
-    if(exits){
-        return res.json({success:false, message:'User already exits'})
+    const exits = await userModel.findOne({ email });
+    if (exits) {
+      return res.json({ success: false, message: "User already exits" });
     }
-   }
 
-   //validation email format and strog password
+    //validation email format and strog password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please Enter a valid email",
+      });
+    }
+    if (!validator.isStrongPassword(password)) {
+      return res.json({
+        success: false,
+        message:
+          "Password must contain at least 1 lowercase, 1 uppercase, 1 number and 1 special character",
+      });
+    }
 
-}
+    //hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+    res.json({ success: true, message: "User created successfully", token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //route for admin login
 
-const adminLogin = async (req, res) => {    
+const adminLogin = async (req, res) => {};
 
-}
-
-export {loginUser, registerUser,adminLogin}
+export { loginUser, registerUser, adminLogin };
