53f9b6215f1c3107baf2b4b375eb4dda3567a362
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-10 11:11:45 +0530

    change hero image

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import {assets} from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div className='home-hero flex flex-col sm:flex-row border border-gray-400'>
+    <div className='home-hero flex flex-col border border-gray-400 sm:min-h-[320px] sm:flex-row'>
       
       {/* Left Side Hero */}
       <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
@@ -32,9 +32,9 @@ const Hero = () => {
      
       
       <img
-        src={assets.hero_img}
-        className='w-fit sm:w-1/2'
-        alt="hero-img"
+        src={assets.heroimg}
+        className='w-fit  sm:w-1/2 sm:object-fill sm:object-center'
+        alt="OIP"
       />
 
     </div>
